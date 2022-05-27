78654aa3768910dd1d180874d78d3f52b0453a23
Jean Meira <jean.meira10@hotmail.com>
2022-05-27 19:12:44 -0300

    feat: add function#1

--- funcoes/funcao_1.js
+++ funcoes/funcao_1.js
@@ -1,1 +1,35 @@
 console.log("teste");
+
+//@ function declaration
+function bomDia(params) {
+  console.log("Bom dia!");
+}
+
+bomDia();
+
+//@ function expression
+const boaTarde = function () {
+  console.log("Boa tarde");
+};
+
+boaTarde();
+
+function somar(a, b) {
+  return a + b;
+}
+
+/*
+  _ uma funcão pode receber quantos parâmetros quiser,
+  _ não alerta se tiver mais ou menos parâmetros.
+  _ se passar mais irá ignorar, se passar menos receberá NaN nesse caso.
+  _ A não ser que atribua um parâmetro formal na declaração da função;
+*/
+
+let resultado = somar(3, 4);
+console.log("resultado com dois parâmetros", resultado);
+
+resultado = somar(3, 4, 5, 6);
+console.log("resultado com mais parâmetros", resultado);
+
+resultado = somar(3);
+console.log("resultado com menos parâmetros", resultado);
