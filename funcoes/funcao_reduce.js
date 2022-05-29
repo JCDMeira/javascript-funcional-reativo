57ce3261aee3fa3aabf4ccec101dcd7240eeb0c1
Jean Meira <jean.meira10@hotmail.com>
2022-05-29 14:51:10 -0300

    feat: add function reduce#1

---
+++ funcoes/funcao_reduce.js
@@ -0,0 +1,10 @@
+/*
+  @ O reduce pode transformar um array em qualquer coisa.
+  @ sendo flexível e quem vai determinar a transformação é a função de callback
+
+  _ pode se passar para o reduce uma função e o valor inicial
+  _ se não passar um valor inicial, o primeiro elemento se torna o inicial
+  ! o valor inicial tem que ser algo que não influencia no resultado final
+
+  _ a funçao recebe o acc (acumulador) e o elemento (valor / el)
+*/
