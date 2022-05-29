bb77c719f51c4fa9f5e9dea4d267cff409bc4c07
Jean Meira <jean.meira10@hotmail.com>
2022-05-29 14:22:41 -0300

    feat: add function map#2

--- funcoes/funcao_map.js
+++ funcoes/funcao_map.js
@@ -15,3 +15,20 @@ const mapFunc = (n, i, a) =>
     n * 2 + i + a.length
   }`;
 console.log(nums.map(mapFunc));
+
+console.log("-------------------------------");
+
+const carrinho = [
+  { nome: "Caneta", qtde: 10, preco: 7.99 },
+  { nome: "Impressora", qtde: 0, preco: 649.5 },
+  { nome: "Caderno", qtde: 4, preco: 27.1 },
+  { nome: "Lapis", qtde: 3, preco: 5.82 },
+  { nome: "Tesoura", qtde: 1, preco: 19.2 },
+];
+
+const getNome = (item) => item.nome;
+console.log(carrinho.map(getNome));
+
+const getTotal = (item) => item.qtde * item.preco;
+const totalPorItem = carrinho.map(getTotal);
+console.log(totalPorItem);
