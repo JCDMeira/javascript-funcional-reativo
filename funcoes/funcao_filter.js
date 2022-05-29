9673685f4926fdf0c1aada2f13862c6b1e3eae85
Jean Meira <jean.meira10@hotmail.com>
2022-05-29 14:38:19 -0300

    feat: add function filter#2

--- funcoes/funcao_filter.js
+++ funcoes/funcao_filter.js
@@ -5,3 +5,18 @@
   _ A função de callback do filter irá retornar true ou false,
   _ se der verdadeiro o elemento será incluido no novo array
 */
+
+const carrinho = [
+  { nome: "Caneta", qtde: 10, preco: 7.99 },
+  { nome: "Impressora", qtde: 0, preco: 649.5 },
+  { nome: "Caderno", qtde: 4, preco: 27.1 },
+  { nome: "Lapis", qtde: 3, preco: 5.82 },
+  { nome: "Tesoura", qtde: 1, preco: 19.2 },
+];
+
+const qatMaiorQueZero = (item) => item.qtde > 0;
+const getNome = (item) => item.nome;
+
+const itensValidos = carrinho.filter(qatMaiorQueZero);
+const nomesItensValidos = carrinho.filter(qatMaiorQueZero).map(getNome);
+console.log(itensValidos);
