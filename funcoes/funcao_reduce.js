82854473fd632e3ba1c67d34b3f56e578f1a85de
Jean Meira <jean.meira10@hotmail.com>
2022-05-29 14:56:11 -0300

    feat: add function reduce#2

--- funcoes/funcao_reduce.js
+++ funcoes/funcao_reduce.js
@@ -8,3 +8,17 @@
 
   _ a funçao recebe o acc (acumulador) e o elemento (valor / el)
 */
+
+const carrinho = [
+  { nome: "Caneta", qtde: 10, preco: 7.99 },
+  { nome: "Impressora", qtde: 0, preco: 649.5 },
+  { nome: "Caderno", qtde: 4, preco: 27.1 },
+  { nome: "Lapis", qtde: 3, preco: 5.82 },
+  { nome: "Tesoura", qtde: 1, preco: 19.2 },
+];
+
+const getTotal = (item) => item.qtde * item.preco;
+const somar = (acc, el) => acc + el;
+
+const totalGeral = carrinho.map(getTotal).reduce(somar);
+console.log(totalGeral);
