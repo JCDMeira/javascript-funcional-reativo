9869ab2be6cd97fb7afd895d97911a73b9aad2e3
Jean Meira <jean.meira10@hotmail.com>
2022-05-29 14:34:00 -0300

    feat: add function filter#1

---
+++ funcoes/funcao_filter.js
@@ -0,0 +1,7 @@
+/*
+  @ O filter gera um novo array, mas terá o número de elementos
+  @ igual ou menor que o número de array original.
+
+  _ A função de callback do filter irá retornar true ou false,
+  _ se der verdadeiro o elemento será incluido no novo array
+*/
