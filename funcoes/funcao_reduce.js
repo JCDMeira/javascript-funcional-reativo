f41be70121d5e607b4dc0cde8d9dec5ba710d8a2
Jean Meira <jean.meira10@hotmail.com>
2022-05-30 20:34:44 -0300

    feat: add function reduce#3

--- funcoes/funcao_reduce.js
+++ funcoes/funcao_reduce.js
@@ -22,3 +22,19 @@ const somar = (acc, el) => acc + el;
 
 const totalGeral = carrinho.map(getTotal).reduce(somar);
 console.log(totalGeral);
+
+Array.prototype.myReduce = function (fn, initial) {
+  let acc = initial;
+  for (let i = 0; i < this.length; i++) {
+    if (!acc && i === 0) {
+      acc = this[i];
+    } else {
+      acc = fn(acc, this[i], i, this);
+    }
+  }
+  return acc;
+};
+
+const totalGeral2 = carrinho.map(getTotal).myReduce(somar);
+
+console.log(totalGeral2);
