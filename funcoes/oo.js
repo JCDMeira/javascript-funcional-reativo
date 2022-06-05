338e25dc9ca65f40dfb5e0b69c0d52fd7b84cea2
Jean Meira <jean.meira10@hotmail.com>
2022-06-05 15:33:21 -0300

    feat: add OO #3

--- funcoes/oo.js
+++ funcoes/oo.js
@@ -54,3 +54,45 @@ console.log(p4.nome);
 p4.nome = "Geladeira1";
 console.log(p4.nome);
 console.log(p4.precoFinal);
+
+//@ função construtora
+
+function Produto3(nome, preco, desc = 0.15) {
+  this._nome = nome;
+  this._preco = preco;
+  this._desc = desc;
+
+  this.precoFinal = () => this._preco * (1 - this._desc);
+}
+
+Produto3.prototype.log = function () {
+  console.log(`Nome: ${this._nome} Preço: R$ ${this._preco}`);
+};
+
+Object.defineProperty(Produto3.prototype, "desc", {
+  get: function () {
+    return this._desc;
+  },
+
+  set: function (novodesc) {
+    if (novodesc >= 0 && novodesc <= 1) {
+      this._desc = novodesc;
+    }
+    return "desconto atualizado com sucesso";
+  },
+});
+
+Object.defineProperty(Produto3.prototype, "descString", {
+  get: function () {
+    return `${this._desc * 100}% de desconto`;
+  },
+});
+
+const p5 = new Produto3("Caneta", 8.59);
+const p6 = new Produto3("Geladeira", 3000);
+
+console.log(p6.precoFinal());
+p6.log();
+console.log(p6.desc);
+p6.desc = 0.3;
+console.log(p6.descString);
