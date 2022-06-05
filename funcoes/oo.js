3b7686bb8bc6dcf0c2b8aa6970894887f76da69c
Jean Meira <jean.meira10@hotmail.com>
2022-06-05 15:22:36 -0300

    feat: add OO #2

--- funcoes/oo.js
+++ funcoes/oo.js
@@ -1,3 +1,5 @@
+//@ função construtora
+
 function Produto(nome, preco, desc = 0.15) {
   this.nome = nome;
   this.preco = preco;
@@ -10,3 +12,45 @@ const p1 = new Produto("Caneta", 8.59);
 const p2 = new Produto("Geladeira", 3000);
 
 console.log(p2.precoFinal());
+
+/*
+  _ usar get acessa função como se fosse atributo normal
+*/
+
+class Produto1 {
+  constructor(nome, preco, desc = 0.15) {
+    this._nome = nome;
+    this._preco = preco;
+    this.desc = desc;
+  }
+
+  get nome() {
+    return this._nome;
+  }
+
+  set nome(novoNome = this._nome) {
+    this._nome = novoNome;
+  }
+
+  get preco() {
+    return this._preco;
+  }
+
+  set preco(novoPreco = this._preco) {
+    if (novoPreco >= 0) {
+      this._preco = novoPreco;
+    }
+  }
+
+  get precoFinal() {
+    return this.preco * (1 - this.desc);
+  }
+}
+
+const p3 = new Produto1("Caneta", 10);
+const p4 = new Produto1("Geladeira", 10000);
+
+console.log(p4.nome);
+p4.nome = "Geladeira1";
+console.log(p4.nome);
+console.log(p4.precoFinal);
