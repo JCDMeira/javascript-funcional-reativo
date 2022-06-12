7f6fd60a7f481e81fb8967588b14243fcd7a63c3
Jean Meira <jean.meira10@hotmail.com>
2022-06-12 16:40:59 -0300

    refactor: remove let's

--- projeto_1/funcoes.js
+++ projeto_1/funcoes.js
@@ -4,9 +4,11 @@ const path = require("path");
 function lerDiretorio(caminho) {
   return new Promise((resolve, reject) => {
     try {
-      let arquivos = fs.readdirSync(caminho);
-      arquivos = arquivos.map((arquivo) => path.join(caminho, arquivo));
-      resolve(arquivos);
+      const arquivos = fs.readdirSync(caminho);
+      const arquivosCompletos = arquivos.map((arquivo) =>
+        path.join(caminho, arquivo)
+      );
+      resolve(arquivosCompletos);
     } catch (e) {
       reject(e);
     }
@@ -54,11 +56,9 @@ function removerElementosSeApenasNumero(array) {
 function removerSimbolos(simbolos) {
   return function (array) {
     return array.map((el) => {
-      let textoSemSimbolos = el;
-      simbolos.forEach((simbolo) => {
-        textoSemSimbolos = textoSemSimbolos.split(simbolo).join("");
-      });
-      return textoSemSimbolos;
+      return simbolos.reduce((acc, simbolo) => {
+        return acc.split(simbolo).join("");
+      }, el);
     });
   };
 }
