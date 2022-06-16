bfecad5ba09bdc35aa2e930394c62623af17c4ca
Jean Meira <jean.meira10@hotmail.com>
2022-06-15 22:00:25 -0300

    feact: add immutability#2

--- funcional/imutabilidade.js
+++ funcional/imutabilidade.js
@@ -5,3 +5,27 @@ function ordenar(array) {
 const nums = Object.freeze([3, 1, 7, 9, 4, 6]); //_ não permite alteração
 const numsOrdenados = ordenar(nums);
 console.log(nums, numsOrdenados);
+
+console.log("-------------------");
+
+const nums2 = [4, 8, 3, 2, 9, 1, 9, 3];
+
+//#1 dados mutáveis
+// let total = 0;
+// for (let i = 0; i < nums2.length; i++) {
+//   total += nums2[i];
+// }
+
+//#2 dados imutáveis - Reduce
+// const somar = (a, b) => a + b;
+// const total = nums2.reduce(somar);
+
+//#3 dados imutáveis - Recursividade
+function somarArray(array, total = 0) {
+  if (array.length === 0) {
+    return total;
+  }
+  return somarArray(array.slice(1), total + array[0]);
+}
+const total = somarArray(nums2);
+console.log(total);
