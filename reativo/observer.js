a01392c8096035d84ec26a29ec1d1d4e8efa10e6
Jean Meira <jean.meira10@hotmail.com>
2022-06-17 17:58:53 -0300

    feat: add observer#1

---
+++ reativo/observer.js
@@ -0,0 +1,17 @@
+//* padrão observer
+
+/*
+  _ Padrão mais usado da web
+  _ base reativa
+
+  _passos
+  - repassa/delega a resposabilidade da notificação
+  - o evento acontece
+    - callback para 'avisar' ou 'notificar'
+    - subject, a parte mais adequada para criar/gerar o aviso
+  - o subject gera o callback
+  - o executador executa todo o processo necessário.
+
+  _ Padrão observer
+    - padrão orientado a eventos
+*/
