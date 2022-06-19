7e567277995859d8612bcf055fb38ebf599e2e9e
Jean Meira <jean.meira10@hotmail.com>
2022-06-19 19:06:11 -0300

    feat: add data stream#3

---
+++ reativo/stream_dados3.js
@@ -0,0 +1,15 @@
+const { interval } = require("rxjs");
+
+const gerarNumeros = interval(500);
+
+const sub1 = gerarNumeros.subscribe((num) => console.log(Math.pow(2, num)));
+
+setTimeout(() => {
+  sub1.unsubscribe();
+}, 8000);
+
+const sub2 = gerarNumeros.subscribe(console.log);
+
+setTimeout(() => {
+  sub2.unsubscribe();
+}, 6000);
