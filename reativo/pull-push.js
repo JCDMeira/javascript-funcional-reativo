e6561519ec6bcecf47bce494cacae320f6e1bbfa
Jean Meira <jean.meira10@hotmail.com>
2022-06-23 19:20:08 -0300

    feat: add notes about pull vs push

---
+++ reativo/pull-push.js
@@ -0,0 +1,9 @@
+/*
+
+    x     single      multi
+  Pull    function    Iterator
+  Push    promise     observable
+
+  _ o pull é quando se chama de forma ativa para pegar as informações
+  _ o push é quando quem produz os dados envia
+*/
